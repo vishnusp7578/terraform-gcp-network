cd68b0eefb69993cca6df32be892d125c51f1001
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 01:01:18 +0530

    Create variables.tf

---
+++ modules/vmrouter/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The region for the router VM"
+}
+
+variable "subnet_links" {
+  type        = map(string)
+  description = "Map of subnet names to their self_links (from VPC module)"
+}
+
+variable "vm_sa_email" {
+  type        = string
+  description = "The email of the service account to attach to the router"
+}
