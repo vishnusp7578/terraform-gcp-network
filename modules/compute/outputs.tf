18172d42e3e39138430aa3eaede038b8ea8869f3
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 23:26:06 +0530

    Update outputs.tf

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,9 @@
-output "internal_ip" { value = google_compute_instance.vm.network_interface.0.network_ip }
+output "internal_ips" {
+  description = "The internal IP addresses of the instances"
+  # We use a 'for' loop to iterate over the collection of instances
+  value = {
+    for instance in google_compute_instance.vm : 
+    instance.name => instance.network_interface[0].network_ip
+  }
+}
+
