00472d1372ec58ef934c70fffb071f125f5794af
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:26:04 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,13 +1,16 @@
-resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
+resource "google_compute_network" "main" {
+  name                    = var.network_name
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnets" {
-  for_each = var.subnets
-
+  for_each      = var.subnet_config
   name          = each.key
   ip_cidr_range = each.value.cidr
   region        = var.region
-  network       = google_compute_network.vpc.id
+  network       = google_compute_network.main.id
+  private_ip_google_access = true
 }
+
+output "network_id" { value = google_compute_network.main.id }
+output "subnet_ids" { value = { for k, v in google_compute_subnetwork.subnets : k => v.id } }
