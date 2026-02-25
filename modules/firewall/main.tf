70a935c136814d7a856ee80185c5f237cbaa7275
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:41:03 +0530

    Update main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,23 +1,23 @@
 resource "google_compute_firewall" "ssh" {
   name    = "allow-ssh"
   network = var.network_id
-  allow { protocol = "tcp"; ports = ["22"] }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
   source_ranges = [var.admin_ip]
   target_tags   = ["ssh-enabled"]
 }
 
-resource "google_compute_firewall" "http" {
-  name    = "allow-web"
+resource "google_compute_firewall" "icmp" {
+  name    = "allow-internal-icmp"
   network = var.network_id
-  allow { protocol = "tcp"; ports = ["80", "443"] }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["web-server"]
-}
 
-# Explicitly allow internal ICMP (Ping)
-resource "google_compute_firewall" "internal_ping" {
-  name    = "allow-internal-ping"
-  network = var.network_id
-  allow { protocol = "icmp" }
+  allow {
+    protocol = "icmp"
+  }
+
   source_ranges = ["10.0.0.0/16"] 
 }
