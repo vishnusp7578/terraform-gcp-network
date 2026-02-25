15044ed1b01b1092c0615a92cc316bbf10926518
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:44:45 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,24 +1,30 @@
+# This name "sa" must match the reference below
+resource "google_service_account" "sa" {
+  account_id   = "${var.name}-sa"
+  display_name = "Service Account for ${var.name}"
+}
+
 resource "google_compute_instance" "vm" {
   name         = var.name
-  machine_type = "e2-micro"
+  machine_type = var.machine_type # Use the variable passed from root
   zone         = var.zone
   tags         = var.tags
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.vm_image # Use the variable passed from root
     }
   }
 
   network_interface {
     subnetwork = var.subnet_id
-    access_config {
-      # Leaving this empty assigns an ephemeral external IP
-    }
+    access_config {}
   }
 
   service_account {
-    email  = google_service_account.sa.email
+    # This must match the resource block name above
+    email  = google_service_account.sa.email 
     scopes = ["cloud-platform"]
   }
 }
+
