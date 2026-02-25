1e65403aca69f2132ac270284244a1003189f4fb
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 03:14:00 +0530

    Create variables.tf

---
+++ modules/nat/variables.tf
@@ -0,0 +1,5 @@
+variable "project_id"       { type = string }
+variable "region"           { type = string }
+variable "vpc_id"           { type = string }
+variable "network_name"     { type = string }
+variable "target_subnet_id" { type = string } 
