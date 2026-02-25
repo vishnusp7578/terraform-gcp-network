3f14c7c854ded350d0833deac213426d1395dae4
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:50:14 +0530

    Update variables.tf

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -7,3 +7,10 @@ variable "vm_image" {
   type        = string
   description = "The OS image for the VM"
 }
+
+variable "subnet_config" {
+  description = "A map of subnet names to their CIDR blocks"
+  type = map(object({
+    cidr = string
+  }))
+}
