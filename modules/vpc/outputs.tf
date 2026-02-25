28d13dd25f2fad8d03067eabd5d3a7993ace2418
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 00:46:08 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,3 +5,8 @@ output "vpc_id" {
 output "subnet_links" {
   value = { for k, v in google_compute_subnetwork.subnet : k => v.self_link }
 }
+
+output "vpc_name" {
+  value       = google_compute_network.vpc.name
+  description = "The name of the VPC"
+}
