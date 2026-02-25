f5e4d10c00a94ca8a87022fec3c77a9dbf5f487f
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 01:00:38 +0530

    Update main.tf

--- modules/vmrouter/main.tf
+++ modules/vmrouter/main.tf
@@ -1,1 +1,24 @@
+resource "google_compute_instance" "router_vm" {
+  name         = "router-vm"
+  machine_type = "e2-medium" # Routing can be CPU intensive; avoid micro for this
+  zone         = "${var.region}-a"
 
+  #This allows the VM to process traffic destined for other IPs
+  can_ip_forward = true 
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnet_links["subnet-a"] # Place it in your internal subnet
+    # No access_config needed if it's purely internal
+  }
+
+  service_account {
+    email  = var.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+}
