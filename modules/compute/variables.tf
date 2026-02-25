3d960ca03f2a95bc39a9298fed56b93ec640bb3c
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:45:13 +0530

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,4 +1,7 @@
-variable "name"      { type = string }
-variable "zone"      { type = string }
-variable "subnet_id" { type = string }
-variable "tags"      { type = list(string) }
+variable "name"         { type = string }
+variable "zone"         { type = string }
+variable "subnet_id"    { type = string }
+variable "tags"         { type = list(string) }
+variable "machine_type" { type = string }
+variable "vm_image"     { type = string }
+
