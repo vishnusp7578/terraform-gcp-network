db53fbcfabc0df1b667d40ea3ca8a48cecd6bdc3
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 23:18:44 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,17 +1,20 @@
 module "vpc" {
-  source       = "../../modules/vpc"
-  project_id   = var.project_id
-  region       = var.region
-  vpc_name     = "prod-vpc"
-  subnets      = var.subnets
+  source     = "../../modules/vpc"
+  project_id = var.project_id
+  region     = var.region
+  vpc_name   = "prod-vpc"
+  subnets    = var.subnets
 }
 
 module "compute" {
-  source          = "../../modules/compute"
-  project_id      = var.project_id
-  vpc_id          = module.vpc.vpc_id
-  subnet_links    = module.vpc.subnet_links
-  machine_type    = "e2-micro"
+  source       = "../../modules/compute"
+  project_id   = var.project_id
+  region       = var.region
+  vpc_id       = module.vpc.vpc_id
+  subnet_links = module.vpc.subnet_links
+  machine_type = var.machine_type
+  # If your compute module has a vm_image variable, add it here:
+  # vm_image   = var.vm_image 
 }
 
 
