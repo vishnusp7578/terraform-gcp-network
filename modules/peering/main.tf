e7b892ce9c0381913c65f5931f52cd7c6a361601
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 04:08:19 +0530

    Create main.tf

---
+++ modules/peering/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network_peering" "peering_a_to_b" {
+  name         = "${var.vpc_a_name}-to-${var.vpc_b_name}"
+  network      = var.vpc_a_id
+  peer_network = var.vpc_b_id
+}
+
+resource "google_compute_network_peering" "peering_b_to_a" {
+  name         = "${var.vpc_b_name}-to-${var.vpc_a_name}"
+  network      = var.vpc_b_id
+  peer_network = var.vpc_a_id
+}
