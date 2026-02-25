8f3915785f2d8c76eb4e497ea30e2dc15a7fe206
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 01:26:41 +0530

    Update main.tf

--- modules/routes/main.tf
+++ modules/routes/main.tf
@@ -1,1 +1,8 @@
-
+resource "google_compute_route" "custom_route" {
+  name                   = var.route_name
+  dest_range             = var.dest_range
+  network                = var.network_name
+  next_hop_instance      = var.next_hop_instance
+  next_hop_instance_zone = var.next_hop_instance_zone
+  priority               = var.priority
+}
