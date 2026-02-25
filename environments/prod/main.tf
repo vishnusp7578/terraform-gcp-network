897d4be4defbfc7996922a1beee75710f0f3f678
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 22:56:24 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,37 +1,17 @@
 module "vpc" {
-  source       = "../../modules/vpc"
-  network_name = "prod-vpc"
-  region       = "us-central1"
-  subnet_config = {
-    "subnet-a" = { cidr = "10.0.1.0/24" }
-    "subnet-b" = { cidr = "10.0.2.0/24" }
-  }
+  source       = "./modules/vpc"
+  project_id   = var.project_id
+  region       = var.region
+  vpc_name     = "prod-vpc"
+  subnets      = var.subnets
 }
 
-module "firewall" {
-  source     = "../../modules/firewall"
-  network_id = module.vpc.network_id
-  admin_ip   = var.my_ip
-}
-
-module "vm_a" {
-  source       = "../../modules/compute"
-  name         = "vm-a"
-  zone         = "${var.region}-a"
-  subnet_id    = module.vpc.subnet_ids["subnet-a"]
-  tags         = ["ssh-enabled"]
-  machine_type = var.machine_type
-  vm_image     = var.vm_image
-}
-
-module "vm_b" {
-  source       = "../../modules/compute"
-  name         = "vm-b"
-  zone         = "${var.region}-b"
-  subnet_id    = module.vpc.subnet_ids["subnet-b"]
-  tags         = ["ssh-enabled"]
-  machine_type = var.machine_type
-  vm_image     = var.vm_image
+module "compute" {
+  source          = "./modules/compute"
+  project_id      = var.project_id
+  vpc_id          = module.vpc.vpc_id
+  subnet_links    = module.vpc.subnet_links
+  machine_type    = "e2-micro"
 }
 
 
