e8137b169aad63df268f6f0ee52340622fe7f79d
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-25 23:10:24 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source       = "./modules/vpc"
+  source       = "../../modules/vpc"
   project_id   = var.project_id
   region       = var.region
   vpc_name     = "prod-vpc"
@@ -7,11 +7,12 @@ module "vpc" {
 }
 
 module "compute" {
-  source          = "./modules/compute"
+  source          = "../../modules/compute"
   project_id      = var.project_id
   vpc_id          = module.vpc.vpc_id
   subnet_links    = module.vpc.subnet_links
   machine_type    = "e2-micro"
 }
+
 
 
