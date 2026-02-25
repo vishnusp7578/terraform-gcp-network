8877c9138f6dea26b5de5efc8135ddc49992ad12
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 00:42:05 +0530

    Update variables.tf

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,1 +1,19 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
 
+variable "network_name" {
+  type        = string
+  description = "The name of the VPC network where rules will be applied"
+}
+
+variable "my_ip" {
+  type        = string
+  description = "The public IP address (CIDR format) allowed to SSH into the VMs"
+}
+
+variable "vm_sa_email" {
+  type        = string
+  description = "The email of the service account used by the VMs (for identity-based firewalling)"
+}
